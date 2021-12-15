4855e16c73609f2e44a9e13e3ce2dec707ae634f
s137092 <egawa.koichi@sharp.co.jp>
2021-12-15 16:50:36 +0900

    初回コミット

---
+++ SlideShowApp/Effector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+
+namespace SlideShowApp.ImageMaker
+{
+    internal class Effector
+    {
+        public double fps_{get;}
+        public Mat previewImage { get; }
+        public Mat nextImage { get; }
+
+        public int width_ { get; }
+        public int height_ { get; }
+
+        public Effector(Mat previewImage, Mat nextImage, )
+    }
+}
